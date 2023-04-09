f8ec7cdda8fa1750be4928cbe4b6ace4c9e638f6
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-04-09 13:03:35 +0200

    Create our basic blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # get "blog_posts/new", to: "blog_posts#new", as: :new_blog_post
+  # get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  # patch "/blog_posts/:id", to: "blog_posts#update"
+  
+  # delete "/blog_posts/:id", to: "blog_posts#destroy"
+  # get "blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
+  # post "/blog_posts", to: "blog_posts#create", as: :blog_posts
+ 
+  resources :blog_posts
+
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "blog_posts#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.where(email: "moenur90@gmail.com").first_or_initialize
+
+user.update!(
+  password: "password",
+  password_confirmation: "password"
+)
